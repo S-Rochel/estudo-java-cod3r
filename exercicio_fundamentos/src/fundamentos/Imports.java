860370c3fe49f6c4e0bdcc18c2155ef08f832b52
Sandro Rochel <sandro.rochel@gmail.com>
2022-02-06 18:20:13 -0300

    Aula usando o import

---
+++ exercicio_fundamentos/src/fundamentos/Imports.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+// Os imports ficam fora da classe.
+// Ctrl + space para importar.
+
+import java.util.Date;
+
+// import javax.swing.JButton;
+
+
+public class Imports {
+
+    public static void main(String[] args) {
+
+        Date d = new Date();
+        System.out.println(d);
+
+        // JButton botao = new JButton();
+    }
+}
