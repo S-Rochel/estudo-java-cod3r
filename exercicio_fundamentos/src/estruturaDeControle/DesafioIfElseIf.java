209769609b4985bcc0eaee7feab58c221d9dffd3
Sandro Rochel <sandro.rochel@gmail.com>
2022-02-15 21:49:19 -0300

    Desafio if else if

---
+++ exercicio_fundamentos/src/estruturaDeControle/DesafioIfElseIf.java
@@ -0,0 +1,35 @@
+package estruturaDeControle;
+
+import java.util.Scanner;
+
+public class DesafioIfElseIf {
+    
+    public static void main(String[] args) {
+        
+        // Desafio receber o dia da semana por extenso e retornar um valor númerico referente ao dia da semana.
+
+        // Entrada de dados.
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Informe o dia da semana por extenso: ");
+        String diaSemana = entrada.next();
+
+        // Lógica.
+        if ("DOMINGO".equals(diaSemana.toUpperCase())) {
+            System.out.println(1);
+        } else if ("SEGUNDA-FEIRA".equals(diaSemana.toUpperCase())) {
+            System.out.println(2);
+        } else if ("TERÇA-FEIRA".equals(diaSemana.toUpperCase())) {
+            System.out.println(3);
+        } else if ("QUARTA-FEIRA".equals(diaSemana.toUpperCase())) {
+            System.out.println(4);
+        } else if ("QUINTA-FEIRA".equals(diaSemana.toUpperCase())) {
+            System.out.println(5);
+        } else if ("SEXTA-FEIRA".equals(diaSemana.toUpperCase())) {
+            System.out.println(6);
+        } else if ("SABADO".equals(diaSemana.toUpperCase())) {
+            System.out.println(7);
+        } else {
+            System.out.println("Erro ao digitar o dia da semana!");
+        }     
+    }         
+}
