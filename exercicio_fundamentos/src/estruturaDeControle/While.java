dc10ac7651cd3f27d80b286a609449caf08444ec
Sandro Rochel <sandro.rochel@gmail.com>
2022-02-17 21:29:50 -0300

    Aula While indeterminado

--- exercicio_fundamentos/src/estruturaDeControle/While.java
+++ exercicio_fundamentos/src/estruturaDeControle/While.java
@@ -1,5 +1,7 @@
 package estruturaDeControle;
 
+import java.util.Scanner;
+
 public class While {
     
     public static void main(String[] args) {
@@ -10,5 +12,21 @@ public class While {
             System.out.printf("Este é o %dº laço.\n", contador);
             contador ++;
         }
+
+
+        // while indeterminado.
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Informe uma cor ou \"sair\", para finalizar.");
+        String palavra = entrada.next();
+        System.out.println("Sua resposta foi: " + palavra);
+
+        while(!palavra.equalsIgnoreCase("sair")) {
+            
+            System.out.println("Informe uma cor ou \"sair\", para finalizar.");
+            palavra = entrada.next();
+            System.out.println("Sua resposta foi: " + palavra);
+        }
+
+        entrada.close();
     }
 }
