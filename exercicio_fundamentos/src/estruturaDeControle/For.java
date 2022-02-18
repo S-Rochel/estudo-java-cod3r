fd3abaf000ac92b77260de2296bdbdbf9707e351
Sandro Rochel <sandro.rochel@gmail.com>
2022-02-17 21:05:00 -0300

    Aula laço For

---
+++ exercicio_fundamentos/src/estruturaDeControle/For.java
@@ -0,0 +1,14 @@
+package estruturaDeControle;
+
+public class For {
+    
+    public static void main(String[] args) {
+        
+        // for 1.
+        for(int c = 1; c <= 4; c++) {
+            System.out.printf("Este é o %dº laço.\n", c);
+        }
+
+        
+    }
+}
