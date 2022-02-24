04fb5c5b3131408eb25da2a01e5734d34a82adea
Sandro Rochel <sandro.rochel@gmail.com>
2022-02-24 18:30:08 -0300

    Aula foreach

---
+++ exercicio_fundamentos/src/listasArrays/ArrayForeach.java
@@ -0,0 +1,14 @@
+package listasArrays;
+
+public class ArrayForeach {
+
+    public static void main(String[] args) {
+        
+        // Cria e inicializar um array.
+        double[] notas = {9.9, 8.7, 7.2, 9.4};
+
+        for (double d : notas) {
+            System.out.println(d);
+        }
+    }
+}
