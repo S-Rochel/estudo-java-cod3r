3af8336ebc6e775dcb2f95f79fdcb5209a418e24
Sandro Rochel <sandro.rochel@gmail.com>
2022-02-28 09:53:35 -0300

    Aula herança sobscrever método

--- exercicio_fundamentos/src/oo/heranca/Heroi.java
+++ exercicio_fundamentos/src/oo/heranca/Heroi.java
@@ -3,4 +3,19 @@ package oo.heranca;
 public class Heroi extends Jogador {
     
     // Heroi recebe todo o código de Jogador.
+
+    // sobreescrever o método atacar.
+    boolean atacar(Jogador oponente) {
+        int deltax = Math.abs(x - oponente.x);
+        int deltay = Math.abs(y - oponente.y);
+
+        if(deltax == 0 && deltay == 1) {
+            oponente.vida -= 20; // era 10 foi mudado para 20
+            return true;
+        } else if (deltax == 1 && deltay == 0) {
+            oponente.vida -= 10;
+            return true;
+        }
+        return false;
+    }
 }
