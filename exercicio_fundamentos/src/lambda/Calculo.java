9425dc41355be9d2ad09dcb9d6264ae5064a1620
Sandro Rochel <sandro.rochel@gmail.com>
2022-03-10 17:36:02 -0300

    Aula interfaces Funcionais

--- exercicio_fundamentos/src/lambda/Calculo.java
+++ exercicio_fundamentos/src/lambda/Calculo.java
@@ -1,6 +1,6 @@
 package lambda;
 
-public interface Calculo {
-    
-    public abstract double executar(double a, double b);
+@FunctionalInterface // Diz que a interface acita apenas um método isso é necessário para que possamos aplicar uma função Lambda nesta interface.
+public interface Calculo {    
+    public abstract double executar(double a, double b);    
 }
