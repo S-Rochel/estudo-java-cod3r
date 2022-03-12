16b9d0af3f309847fd217cb1a4676809c1480632
Sandro Rochel <sandro.rochel@gmail.com>
2022-03-11 21:20:35 -0300

    Aula Interface Funcional "Function"

---
+++ exercicio_fundamentos/src/lambda/Funcao.java
@@ -0,0 +1,28 @@
+// Programação funcional - Interfaces Funcionais - Function
+package lambda;
+
+import java.util.function.Function;
+
+public class Funcao {
+    
+    public static void main(String[] args) {
+        // Interface funcional "Function" recebe através do generics um valor e retorna um outro valor.
+
+        // Recebendo um Integer e retornando uma String
+        Function<Integer, String> parOuImpar = numero -> numero % 2 == 0 ? "Par" : "Impar";
+        // teste
+        System.out.println(parOuImpar.apply(10));
+
+        // Recebendo uma String e retornando uma String
+        Function<String, String> oResultatoE = valor -> "O resultado é: " + valor;
+        
+        Function<String, String> empolgado = valor -> valor + "!!!";
+
+        // Testando usando encadeamento de funções:
+        String resultadoFinal = parOuImpar
+        .andThen(oResultatoE)
+        .andThen(empolgado)
+        .apply(30);
+        System.out.println(resultadoFinal);
+    }
+}
